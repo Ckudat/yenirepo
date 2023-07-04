9da0903a9fe61732b359de165e983d74ca565742
caner <canerkdt7@gmail.com>
2023-07-04 13:59:43 +0300

    export-function

--- app2.js
+++ app2.js
@@ -3,3 +3,19 @@ const user = {
     imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
     imageSize: 90,
 };
+export default function Profile() {
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize
+                }}
+            />
+        </>
+    );
+}
