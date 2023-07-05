c0762271c6892359b2ce9802fc15f18220671466
caner <canerkdt7@gmail.com>
2023-07-05 11:31:38 +0300

    REST APIs in React

--- languages.js
+++ languages.js
@@ -1,13 +1,37 @@
-const languages = [
-    {"id": 1, "name": "Turkish", "iso_code": "TR"},
-    {"id": 5, "name": "English", "iso_code": "EN"}
-];
-const listItems = languages.map(language =>
-    <li key={language.id}>
-        {language.name}
-    </li>
-);
 
-return (
-    <ul>{listItems}</ul>
-);
+import React, { useEffect, useState } from 'react';
+
+const Language = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await fetch('https://pe8pd46p8363npu5.mlncgs.net/language/list');
+                const data = await response.json();
+                setPosts(data);
+            } catch (error) {
+                console.log(error.message);
+            }
+        };
+
+        fetchPosts();
+    }, []);
+
+    return (
+        <div>
+            <h1>Posts:</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                        <p>User ID: {post.userId}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Language;
