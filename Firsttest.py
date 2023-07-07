6982147214a9dfa81be8fa3e9539766c0153f815
caner <canerkdt7@gmail.com>
2023-07-07 16:38:51 +0300

    aldığım hatalar

---
+++ Firsttest.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  7 14:18:03 2023
+
+@author: monster
+"""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    def __repr__(self):
+        return '<User %r>' % self.username
---
+++ create-db.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  7 15:08:58 2023
+
+@author: monster
+"""
+
+from Firsttest import db
+
+db.create_all()
